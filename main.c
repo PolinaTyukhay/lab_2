70edcba157484376316e0001ce7feffbcbec0a37
Lina-Anna <62033643+Lina-Anna@users.noreply.github.com>
2021-01-02 19:49:59 +0500

    буэээ

--- main.c
+++ main.c
@@ -12,12 +12,27 @@
  
 int i_buf=0, len_buf=0; // счет и длинна буфера 
 char buf[LONG];
+
+int i_Tbuf = 0, len_Tbuf = 0; // счет и длинна буфера 
+char Tbuf[LONG];
+
 FILE* f;
 FILE* new_file;
 
 
 char get_sym() {// получение символа 
-	if ((len_buf == 0)||(i_buf ==len_buf)) { // либо начало либо кнец 
+	
+	if ((len_Tbuf == 0) || (i_Tbuf == len_Tbuf)) { // либо начало либо кoнец 
+		if (len_Tbuf != 0) {
+			len_Tbuf = 0;
+			i_Tbuf = 0;
+		}
+	}
+	else {
+		return(Tbuf[i_Tbuf++]);
+	}
+	
+	if ((len_buf == 0)||(i_buf ==len_buf)) { // либо начало либо кoнец 
 		len_buf=fread(buf, sizeof(char), LONG, f); // считывает с места где остановился 
 		if (len_buf != LONG) {  //если не дошел до конца 
 			if (!feof(f)) {// либо ошибка 
@@ -58,7 +73,7 @@ int main(int argc, char** argv){
 	setlocale(LC_ALL, "Rus");
 	int ii;
 
-	printf("%d\n", argc);
+	//printf("%d\n", argc);
 	if (argc !=5 ) {
 		puts("Неверное количество параметров");
 		puts("параметры запуска :lab_2.exe file_in str1 str2 file_out");
@@ -78,11 +93,12 @@ int main(int argc, char** argv){
 	strcpy_s(old, strlen(argv[2]) + 1, argv[2]);
 	strcpy_s(new, strlen(argv[3]) + 1, argv[3]);
 	strcpy_s(scnd_name, strlen(argv[4]) + 1, argv[4]);
-	printf("%s \t %s \t %s \t %s \n", name, old, new, scnd_name);
+	//printf("%s \t %s \t %s \t %s \n", name, old, new, scnd_name);
 	
 	fopen_s(&new_file, scnd_name, "w");
 
 	int res;
+	int d = 0;
 	res = check(name);
 	if (res == 0){
 		exit(1);
@@ -100,14 +116,29 @@ int main(int argc, char** argv){
 			}
 		}
 		else {
-			for (int j = 0; j < i; j++)// не совпадает => вывести старую 
-			{
-				//printf("%c", old[j]);
-				fprintf_s(new_file, "%c", old[j]);
+			if (i > 0) {
+				fprintf_s(new_file, "%c", old[0]);
+				d = 0;
+				if ((i_Tbuf == 0) && (len_Tbuf == 0)) {
+						d = -1;
+			    }
+				for (int j = 1; j < i; j++)
+				{
+
+					Tbuf[i_Tbuf + j+d] = old[j];
+					len_Tbuf++;
+					//printf("%c", old[j]);
+
+				}
+				Tbuf[i_Tbuf + i+d] = c;
+				len_Tbuf++;
+				
+				i = 0;
+			
 			}
-			i = 0;
-			//printf("%c", c);
-			fprintf_s(new_file, "%c", c);
+			else {
+				fprintf_s(new_file, "%c", c);
+			}
 		}
 		
 	}
@@ -120,6 +151,6 @@ int main(int argc, char** argv){
 	fclose(new_file);
 	
 
-
+	printf("it's all");
 
 }
---
+++ tst.cmd
@@ -0,0 +1,4 @@
+type 111.txt
+Debug\lab_2.exe 111.txt jf "%date% %time%" 123.txt
+type 123.txt
+pause
